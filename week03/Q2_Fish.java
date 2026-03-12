0d46b6e9e3895ada091b8c7ee02827a2e908a944
EK181121 <c113181121@nkust.edu.tw>
2026-03-12 11:47:18 +0800

    update

--- week03/Q2_Fish.java
+++ week03/Q2_Fish.java
@@ -1,1 +1,16 @@
+public class Q2_Fish {
+    String name;
+    double weight;
 
+    public void displayinfo(){
+        System.out.println("This fish's name is："+name+"Weight："+weight+"KG");
+    }
+
+    public static void main(String[] args) {
+        Q2_Fish myfish=new Q2_Fish();
+        myfish.name="Black fish";
+        myfish.weight=250.5;
+
+        myfish.displayinfo();
+    }
+}
